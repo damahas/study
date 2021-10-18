5691246daac8d8f328e40e3d6c394b96f74a6261
damahas <88645644+damahas@users.noreply.github.com>
2021-10-19 00:02:09 +0800

    Update jb_test.js

--- jb_test.js
+++ jb_test.js
@@ -12,7 +12,7 @@ function runSign() {
             'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 14_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.14(0x18000e2d) NetType/WIFI Language/zh_CN',
             'Accept-Language': 'zh-cn'
         },
-        body: {"data":{"projectId":"B79E72A1-2A5C-E311-8D79-0050568001F7"}}
+        body: `{"data":{"projectId":"B79E72A1-2A5C-E311-8D79-0050568001F7"}}`
   },res=>{
     console.log(res);
   });
